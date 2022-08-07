7fb5836004db57d8d6122d36ebadc54bd1f65c8d
Your Name <you@example.com>
2022-08-07 18:23:18 +0100

    add Procfile and update ports for Heroku

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 //conenct to mongodb
 main().catch(err => console.log(err));
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/todolistDB');
+  await mongoose.connect('mongodb+srv://admin-michael:test123@cluster0.b43ktyl.mongodb.net/todolistDB');
 }
 
 //create SCHEMA
@@ -147,6 +147,11 @@ app.get("/about", function(req, res) {
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started successfully");
 });
